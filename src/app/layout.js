4be47d10eb56ec3828549477cf8ad8d9c6cb2f0a
Sachi8565 <101034229+Sachi8565@users.noreply.github.com>
2025-04-29 15:53:36 +0530

    kk

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Mr. Service Expert - Home Appliances Repair Services ",
   description: "Mr. Service Expert is your one-stop solution for home appliances repair services in Delhi. We have 1000+ trusted professionals near you in Delhi to take care of your home appliances.",
+  keywords:"ac repair service,water purifier repair,home appliance repair,refrigerator repair service,washing machine repair,air conditioner maintenance,ro service near me,microwave oven repair,geyser installation service,appliance service center",
   alternates: {
     canonical: "https://www.mrserviceexpert.com/",
   },
