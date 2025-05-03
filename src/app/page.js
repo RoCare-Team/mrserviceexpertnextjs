8ecd4b90237ab05e07300b6f1376bbf35b16aa3d
ro care <sachhidanand.3dlogic@gmail.com>
2025-05-03 13:43:39 +0530

    number added and all other changes done for now

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,17 @@ import React from 'react'
 
 import RepairView from '../app/components/repairs/repairView';
 import ServiceSection from '../app/components/servicesSection/servicesSection';
+const reviewer=[
+    {
+    id:"1",
+    reviewerName:"Rahul",
+    reviewerLocation:"Delhi",
+    reviewerRating:"4.5",
+    reviewerContent:"It's a good Product from Doctor Fresh. Water Purifier price is good. Delive installation was prompt. The water quality is good. The device settings were explained very clearly and setup of the device was very quick and hassle free"
+
+}
+
+]
 
 
 function home() {
@@ -18,7 +29,7 @@ function home() {
                 </div>
 
                 <div className="rightHeroDetails w-5/12 flex flex-col items-start justify-between  pl-2 pr-2">
-                    <div>
+                    <div className='flex flex-col gap-4'>
                         <h1 className='heroHeading'><b>Fast, Trusted Home Appliance service -  Mr. Service Expert</b></h1>
                         <span><a href="tel:+91 9311587744" title='For calling contact +91 9311587744'><button className='book-btn-style' aria-label="Open menu">Call Now</button></a> for reliable home appliance care that protects what matters most  </span>
                         <p className='text-xl'>Ensuring your familys comfort and convenience with expert installation and repair services</p>
@@ -38,11 +49,13 @@ function home() {
                 </div>
             </div>
 
-            <div className=' serviceBannerSection'>
+            {/* <div className=' serviceBannerSection'>
                 <img src="assets/images/home-appliaces.webp" alt='home-appliances' className='serviceBanner'  title='home-appliances' height="auto" width={1314} />
-            </div>
+            </div> */}
 
-            <ServiceSection />
+           <div className='my-6'>
+           <ServiceSection />
+           </div>
             
 
             <div>
@@ -61,7 +74,7 @@ function home() {
             <div className="chooseUs p-4">
 
                 <div className='flex justify-center items-start'>
-                    <h3 className='text-center chooseStar  '>Why Choose Us</h3>
+                    <h3 className='text-center chooseStar  '>Why Choose Mr. Service Expert Services</h3>
                     <span><img src="assets/images/star.webp" alt="choose us" className='w-10' title='choose us'  width="40px" height="auto" /></span>
                 </div>
 
@@ -90,6 +103,16 @@ function home() {
                 </div>
 
             </div>
+ {/* reviewer section */}
+            {/* <div>
+             {reviewer.map(reviews)=>
+                return
+                {
+
+                }
+                
+                }
+            </div> */}
 
 
 
