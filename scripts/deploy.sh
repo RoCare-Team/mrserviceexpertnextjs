3f19e948b91baa11008b4a4f9910b5617eab5bc4
RoCare-Team <sachhidanand.3dlogic@gmail.com>
2026-02-27 16:37:50 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 set -e
 
-# Load NVM properly
-export NVM_DIR="/home/ec2-user/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-nvm use 22
-
-# Ensure PM2 path available
-export PATH=$PATH:/home/ec2-user/.npm-global/bin
-
 cd /home/ec2-user/mrserviceexpertnextjs
 
 npm ci
 npm run build
 
-/home/ec2-user/.npm-global/bin/pm2 delete mrservice || true
-/home/ec2-user/.npm-global/bin/pm2 start npm --name "mrservice" -- start
-/home/ec2-user/.npm-global/bin/pm2 save
+pm2 delete mrservice || true
+pm2 start npm --name "mrservice" -- start
+pm2 save
