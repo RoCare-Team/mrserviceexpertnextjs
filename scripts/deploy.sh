8e7427c5e6b514c89d5ab9ca64aaeea569c158e2
RoCare-Team <sachhidanand.3dlogic@gmail.com>
2026-02-27 16:22:49 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+set -e
+
+# Load NVM (if using)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 22 || true
+
+# Add custom npm global path (VERY IMPORTANT)
+export PATH=$PATH:/home/ec2-user/.npm-global/bin
+
 cd /home/ec2-user/mrserviceexpertnextjs
 
-npm install
+npm ci
 npm run build
 
-pm2 stop mrservice || true
+pm2 delete mrservice || true
 pm2 start npm --name "mrservice" -- start
 pm2 save
