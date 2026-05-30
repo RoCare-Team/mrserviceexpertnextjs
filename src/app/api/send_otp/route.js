151c03a47d2075002cfcd03f3ecbbb95d55e7b27
RoCare-Team <sachhidanand.3dlogic@gmail.com>
2026-05-30 12:34:06 +0530

    add

---
+++ src/app/api/send_otp/route.js
@@ -0,0 +1,17 @@
+export async function POST(request) {
+  const body = await request.json();
+
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_SEND_OTP,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-App-Token": process.env.OTP_TOKEN,
+      },
+      body: JSON.stringify(body),
+    }
+  );
+
+  return Response.json(await response.json());
+}
